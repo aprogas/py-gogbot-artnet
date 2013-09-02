c2049886e4e2a5544ed0c73fb529433ea9b1538f
Jasper Jongmans <git@aprogas.net>
2013-09-02 21:40:25 +0200

    Convert blackout to handle any RGB given as arg

--- allthesame.py
+++ allthesame.py
@@ -2,6 +2,7 @@
 
 # 2013-09-02 Aprogas
 
+import argparse
 import socket
 import time
 
@@ -9,13 +10,19 @@ from artnet import buildPacket
 
 UDP_PORT = 6454
 
+parser = argparse.ArgumentParser(description="Send the same color to all LEDs.")
+parser.add_argument("red"  , type=int, nargs='?', default=0)
+parser.add_argument("green", type=int, nargs='?', default=0)
+parser.add_argument("blue" , type=int, nargs='?', default=0)
+args = parser.parse_args()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)  # Allow broadcast
 
 while (True):
 	data = []
 	for i in xrange(150):
-		data.append((0, 0, 0))  # Black
+		data.append((args.red, args.green, args.blue))
 	sock.sendto(buildPacket(0, data), ("10.0.0.255", UDP_PORT))
 	time.sleep(1)	
 
