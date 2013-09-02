50f69944dfd764691986cdcfdc6b3e5d970e15e3
Jasper Jongmans <git@aprogas.net>
2013-09-02 17:27:53 +0200

    Add barber's pole pattern

--- patterns.py
+++ patterns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
-# 2013-09-02 Aprogas
-
 import math
 
 class PolicePattern:
+	# Rotating blue line with fading trail
 	def __init__(self, backwards=False):
 		self.backwards = backwards
 		self.pos = 0.0
@@ -34,3 +33,31 @@ class PolicePattern:
 			self.pos = 0.0
 	
 		return data
+
+class BarberpolePattern:
+	# Rotating red/white stripes
+	def __init__(self, backwards=False):
+		self.backwards = backwards
+		self.pos = 0.0
+
+	def generate(self):
+		data = []
+		if self.backwards:
+			leds = xrange(150, 0, -1)
+		else:
+			leds = xrange(0, 150, 1)
+		for i in leds:
+			place = (i % 6) + self.pos
+			if (place >= 6):
+				place -= 6
+			if (place < 3):
+				data.append((255, 0, 0))  # red
+			else:
+				data.append((255, 255, 255))  # white
+	
+		self.pos += 1
+		if (self.pos >= 6):
+			self.pos = 0
+	
+		return data
+			
